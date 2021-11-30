a27a9723c19f086b547270839ca33d583879a9c4
vijay kumar <vk77093@gmail.com>
2021-11-30 13:50:42 +0530

    added the sub category repos

---
+++ KalaShree.Controllers/Implemtation/SubCategoryRepo.cs
@@ -0,0 +1,72 @@
+﻿using KalaShree.Controllers.InterFaces;
+using KalaShree.Controllers.SqlDataBase;
+using KalaShree.models.Masters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaShree.Controllers.Implemtation
+{
+    public class SubCategoryRepo : ISubCategoryRepo
+    {
+        private readonly DataBaseContext dataBase;
+
+        public SubCategoryRepo(DataBaseContext dataBase)
+        {
+            this.dataBase = dataBase;
+        }
+        public IEnumerable<SubCategory> GetSubCategories()
+        {
+            return dataBase.SubCategories.ToList();
+        }
+        public void AddSubCategory(SubCategory subCategory)
+        {
+            dataBase.SubCategories.Add(subCategory);
+            dataBase.SaveChanges();
+        }
+        public SubCategory GetSubCategoryById(int subCatId)
+        {
+            var findCateid = dataBase.SubCategories.Find(subCatId);
+            if (findCateid != null)
+            {
+                return findCateid;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void UpdateSubCategory(SubCategory subCategory)
+        {
+            var findUpId = dataBase.SubCategories.Find(subCategory.SubCategoryId);
+            if (findUpId != null)
+            {
+                findUpId.CategoryId = subCategory.CategoryId;
+                findUpId.SubCategoryCode = subCategory.SubCategoryCode;
+                findUpId.SubCategoryName = subCategory.SubCategoryName;
+                findUpId.UpdatedAt = DateTime.Now;
+                dataBase.SaveChanges();
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void DeleteSubcategory(int subCateId)
+        {
+            var subId = dataBase.SubCategories.Find(subCateId);
+            if (subId == null) return;
+            dataBase.SubCategories.Remove(subId);
+            dataBase.SaveChanges();
+        }
+        public bool AlreadyExistSubCategory(SubCategory subCategory)
+        {
+            var SubCatCodeAndName = dataBase.SubCategories.FirstOrDefault(x => x.SubCategoryCode == subCategory.SubCategoryCode
+              || x.SubCategoryName == subCategory.SubCategoryName);
+            if (SubCatCodeAndName != null) return true;
+            return false;
+        }
+    }
+}
---
+++ KalaShree.Controllers/InterFaces/ISubCategoryRepo.cs
@@ -0,0 +1,15 @@
+﻿using KalaShree.models.Masters;
+using System.Collections.Generic;
+
+namespace KalaShree.Controllers.InterFaces
+{
+    public interface ISubCategoryRepo
+    {
+        void AddSubCategory(SubCategory subCategory);
+        bool AlreadyExistSubCategory(SubCategory subCategory);
+        void DeleteSubcategory(int subCateId);
+        IEnumerable<SubCategory> GetSubCategories();
+        SubCategory GetSubCategoryById(int subCatId);
+        void UpdateSubCategory(SubCategory subCategory);
+    }
+}
